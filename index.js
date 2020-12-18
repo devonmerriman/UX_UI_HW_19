d1e3c41ec7fb3758d06d019b25d0b2d791e46794
devonmerriman <68878417+devonmerriman@users.noreply.github.com>
2020-12-17 23:46:58 -0800

    Fade
    added fade effect with jQuery

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("File is loaded correctly");
+
+console.log("here");
+  $(".hero").fadeIn(800);
+
+  $("#case_studies_h2").delay(200).fadeIn(800);
+  $(".line").delay(200).fadeIn(800);
+
+  $("#case_study_1").delay(200).fadeIn(800);
+  $("#case_study_2").delay(200).fadeIn(800);
+  $("#case_study_3").delay(200).fadeIn(800);
