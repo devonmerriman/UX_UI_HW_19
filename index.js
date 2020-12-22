19494ba50af0b9b1ae480c47bb2a42f7f1d8194c
devonmerriman <68878417+devonmerriman@users.noreply.github.com>
2020-12-21 16:32:51 -0800

    messed with jquery

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
-console.log("here");
-  $(".hero").fadeIn(800);
-
+ 
+ $(".hero").fadeIn(800);
   $("#case_studies_h2").delay(200).fadeIn(800);
   $(".line").delay(200).fadeIn(800);
-
   $("#case_study_1").delay(200).fadeIn(800);
   $("#case_study_2").delay(200).fadeIn(800);
   $("#case_study_3").delay(200).fadeIn(800);
+
+
+  $("#case_study_1").animate({top: "100px"});
+  $("#case_study_2").animate({top: "100px"});
+  $("#case_study_3").animate({top: "100px"});
+
