6e08916736e7de9d4fa0cb7d22102c2ef7cd6a2c
devonmerriman <68878417+devonmerriman@users.noreply.github.com>
2020-12-21 22:20:28 -0800

    jquery animation for case studies
    For case studies, got rid of display nones in html and fade-ins in js. made them fade in at the same time as they move upwards.

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 
  
- $(".hero").fadeIn(800);
-  $("#case_studies_h2").delay(200).fadeIn(800);
-  $(".line").delay(200).fadeIn(800);
-  $("#case_study_1").delay(200).fadeIn(800);
-  $("#case_study_2").delay(200).fadeIn(800);
-  $("#case_study_3").delay(200).fadeIn(800);
+  $(".hero").fadeIn(800);
+  $("#case_studies_h2").fadeIn(800);
+  $(".line").fadeIn(800);
 
-
-  $("#case_study_1").animate({top: "100px"});
-  $("#case_study_2").animate({top: "100px"});
-  $("#case_study_3").animate({top: "100px"});
-
+  $("#case_study_1") .animate({top: "-50px", opacity: "1"}, 800);
+  $("#case_study_2") .animate({top: "-50px", opacity: "1"}, 800);
+  $("#case_study_3") .animate({top: "-50px", opacity: "1"}, 800);
